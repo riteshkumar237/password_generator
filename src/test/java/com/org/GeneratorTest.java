6fac3fcf2803c4a01884ba8ee4d6a1bc5a885696
ritesh <riteshkumar237@gmail.com>
2023-01-25 14:02:03 +0530

    created GeneratorTest.java

---
+++ src/test/java/com/org/GeneratorTest.java
@@ -0,0 +1,14 @@
+package com.org;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeneratorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mainLoop() {
+    }
+}
